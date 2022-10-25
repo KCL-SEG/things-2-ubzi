1b68ceb2bb1193d144de848a2cca01a20cc8cd0a
Ubaydullah Naik <ubaydullah.naik@kcl.ac.uk>
2022-10-25 11:16:40 +0100

    Add thing form to home page

--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,22 @@
+from django import forms
 from django.test import TestCase
+from .forms import ThingForm
+from .models import Thing
 
-# Create your tests here.
+class ThingFormTestCase(TestCase):
+
+    def setUp(self):
+        self.form_input = {
+            'name': 'box',
+            'description': 'a box',
+            'quantity': 2,
+        }
+
+    def test_valid_sign_up_form(self):
+        form = ThingForm(data=self.form_input)
+        self.assertTrue(form.is_valid())
+
+    def test_form_uses_model_validation(self):
+        self.form_input['quantity'] = -3
+        form = ThingForm(data=self.form_input)
+        self.assertFalse(form.is_valid())
